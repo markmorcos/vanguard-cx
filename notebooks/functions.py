db85813d598cc1417aac6a6b93056b44c564070c
Mark Morcos <mark.yehia@gmail.com>
2024-11-19 10:37:29 +0100

    feat: clean and populate csv and sql for client information

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,1 +1,17 @@
-#
+def rename_client_columns(df):
+    return df.rename(columns={
+        "client_id": "id",
+        "clnt_tenure_yr": "tenure_years",
+        "clnt_tenure_mnth": "tenure_months",
+        "clnt_age": "age",
+        "gendr": "gender",
+        "num_accts": "num_accounts",
+        "bal": "balance",
+        "calls_6_mnth": "calls_6_months",
+        "logons_6_mnth": "logons_6_months",
+    })
+
+def rename_experiment_columns(df):
+    df_copy = df.copy()
+    df_copy.columns = df.columns.str.lower()
+    return df_copy
